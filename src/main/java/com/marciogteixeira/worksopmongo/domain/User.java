1ccfc9d74bda09b833efcfe27307a7981ba8544a
Marcio Teixeira <marcio.soldado@hotmail.com>
2025-06-09 21:02:08 -0300

    Operacao de instanciacao da base de dados

--- src/main/java/com/marciogteixeira/worksopmongo/domain/User.java
+++ src/main/java/com/marciogteixeira/worksopmongo/domain/User.java
@@ -4,10 +4,13 @@ import java.io.Serializable;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
+@TypeAlias("user")
 @Document(collection="user")
+
+
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
