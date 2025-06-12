4a44642d377780cef1e49a2616b03296bb34fee1
Marcio Teixeira <marcio.soldado@hotmail.com>
2025-06-12 20:31:29 -0300

    Insercao de usuario com POST

--- src/main/java/com/marciogteixeira/workshopmongo/services/UserService.java
+++ src/main/java/com/marciogteixeira/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marciogteixeira.workshopmongo.domain.User;
+import com.marciogteixeira.workshopmongo.dto.UserDTO;
 import com.marciogteixeira.workshopmongo.repository.UserRepository;
 import com.marciogteixeira.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -22,5 +23,15 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+		
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
+		
+	}
 
 }
