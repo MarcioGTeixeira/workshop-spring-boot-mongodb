ecd5e41c52850ccbf4700f0645096135d84af5b2
Marcio Teixeira <marcio.soldado@hotmail.com>
2025-06-24 19:36:18 -0300

    Obtendo um post por id

---
+++ src/main/java/com/marciogteixeira/workshopmongo/resources/PostResource.java
@@ -0,0 +1,29 @@
+package com.marciogteixeira.workshopmongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marciogteixeira.workshopmongo.domain.Post;
+import com.marciogteixeira.workshopmongo.dto.UserDTO;
+import com.marciogteixeira.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+
+	
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id){
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}	
---
+++ src/main/java/com/marciogteixeira/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.marciogteixeira.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marciogteixeira.workshopmongo.domain.Post;
+import com.marciogteixeira.workshopmongo.domain.User;
+import com.marciogteixeira.workshopmongo.dto.UserDTO;
+import com.marciogteixeira.workshopmongo.repository.PostRepository;
+import com.marciogteixeira.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	@Autowired
+	private PostRepository repo;
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+
+}	
