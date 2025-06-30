f26545298ca0db07fee64cec31e072a45c089e15
Marcio Teixeira <marcio.soldado@hotmail.com>
2025-06-30 19:58:12 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/marciogteixeira/workshopmongo/domain/Post.java
+++ src/main/java/com/marciogteixeira/workshopmongo/domain/Post.java
@@ -1,13 +1,17 @@
 package com.marciogteixeira.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 import com.marciogteixeira.workshopmongo.dto.AuthorDTO;
+import com.marciogteixeira.workshopmongo.dto.CommentDTO;
 
 @TypeAlias("post")
 
@@ -21,6 +25,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 		
@@ -75,11 +81,21 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
 
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -91,6 +107,8 @@ public class Post implements Serializable {
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 	
 	
 }
